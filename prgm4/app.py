5b807f153b7b6f9da3ef3c94d701ab89829e9e1a
harsh1n1vg <1ds22cs088@gdsce.edu.in>
2025-05-27 06:33:58 +0530

    update

--- prgm4/app.py
+++ prgm4/app.py
@@ -1,7 +1,11 @@
-from flask import Flask 
+from flask import Flask
+import os
 app=Flask(__name__)
+ENV=os.getenv("ENV","NOT FOUND")
+PASS=os.getenv("PASS","NOT FOUND")
+
 @app.route('/')
-def main():
-  return "HELLO DA"
+def hello():
+    return f"hello {ENV} , {PASS}"
 if __name__=='__main__':
-  app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5000)
